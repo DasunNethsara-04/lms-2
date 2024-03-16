8f84ae848f087de3e570b794310fd7149453e450
DasunNethsara-04 <techsaralk.pro@gmail.com>
2024-03-16 13:20:46 +0530

    Routes added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import About from './Pages/About.jsx'
+import Login from './Pages/Login.jsx'
+import Register from './Pages/Register.jsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
