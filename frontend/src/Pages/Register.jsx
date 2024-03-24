687f00129e06624c866851bd1d64ae9d8eb2abf2
Dasun <techsaralk.pro@gmail.com>
2024-03-24 22:50:13 +0530

    updates for register page

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -65,68 +65,58 @@ function Register() {
 
                       <div className="row mb-4">
                         <div className="col">
-                          <div data-mdb-input-init className="form-outline">
-                            <label
-                              className="form-label"
-                              htmlFor="form3Example1"
-                            >
-                              First name
-                            </label>
-                            <input
-                              type="text"
-                              id="form3Example1"
-                              className="form-control"
-                              name="fname"
-                              required
-                              onChange={(e) => setFirstName(e.target.value)}
-                            />
+                          <div className="form-outline mb-4">
+                            <div className="form-floating mb-3">
+                              <input
+                                type="text"
+                                className="form-control"
+                                id="floatingInput1"
+                                name="firstName"
+                                onChange={(e) => setFirstName(e.target.value)}
+                              />
+                              <label htmlFor="floatingInput1">First Name</label>
+                            </div>
                           </div>
                         </div>
                         <div className="col">
-                          <div data-mdb-input-init className="form-outline">
-                            <label
-                              className="form-label"
-                              htmlFor="form3Example2"
-                            >
-                              Last name
-                            </label>
-                            <input
-                              type="text"
-                              id="form3Example2"
-                              className="form-control"
-                              name="lname"
-                              required
-                              onChange={(e) => setLastName(e.target.value)}
-                            />
+                          <div className="form-outline mb-4">
+                            <div className="form-floating mb-3">
+                              <input
+                                type="text"
+                                className="form-control"
+                                id="floatingInput2"
+                                name="lastName"
+                                onChange={(e) => setLastName(e.target.value)}
+                              />
+                              <label htmlFor="floatingInput2">Last Name</label>
+                            </div>
                           </div>
                         </div>
                       </div>
                       <div className="form-outline mb-4">
-                        <label className="form-label" htmlFor="form2Example17">
-                          Email address
-                        </label>
-                        <input
-                          type="email"
-                          id="form2Example17"
-                          className="form-control"
-                          name="email"
-                          required
-                          onChange={(e) => setEmail(e.target.value)}
-                        />
+                        <div className="form-floating mb-3">
+                          <input
+                            type="email"
+                            className="form-control"
+                            id="floatingInput3"
+                            name="email"
+                            onChange={(e) => setEmail(e.target.value)}
+                          />
+                          <label htmlFor="floatingInput3">Email address</label>
+                        </div>
                       </div>
 
                       <div className="form-outline mb-4">
-                        <label className="form-label" htmlFor="form2Example27">
-                          Password
-                        </label>
-                        <input
-                          type="password"
-                          id="form2Example27"
-                          className="form-control"
-                          name="password"
-                          required
-                          onChange={(e) => setPassword(e.target.value)}
-                        />
+                        <div className="form-floating mb-3">
+                          <input
+                            type="password"
+                            className="form-control"
+                            id="floatingInput4"
+                            name="password"
+                            onChange={(e) => setEmail(e.target.value)}
+                          />
+                          <label htmlFor="floatingInput4">Email address</label>
+                        </div>
                       </div>
                       <div className="pt-1 mb-4">
                         <input
